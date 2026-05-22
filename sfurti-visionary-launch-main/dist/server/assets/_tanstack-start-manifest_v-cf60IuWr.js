d8d609285f2217eb4da919c373fcd8d44adf82f1
Viral Marketing Trends <viralmarketingtrends07@gmail.com>
2026-05-22 16:38:23 +0530

    Delete sfurti-visionary-launch-main directory

--- sfurti-visionary-launch-main/dist/server/assets/_tanstack-start-manifest_v-cf60IuWr.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/chait/Downloads/sfurti-visionary-launch-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-AuBhgZ5L.js"] }, "/": { filePath: "C:/Users/chait/Downloads/sfurti-visionary-launch-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-DGSLp2M3.js"] } }, clientEntry: "/assets/index-AuBhgZ5L.js" });
-export {
-  tsrStartManifest
-};
